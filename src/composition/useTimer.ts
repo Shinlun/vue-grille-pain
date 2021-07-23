b1ad31acef8498c8273a42dc869463d2c2fd5792
Ronan <ronanlet@gmail.com>
2021-07-23 09:45:11 +0200

    Remove console.logs

--- src/composition/useTimer.ts
+++ src/composition/useTimer.ts
@@ -1,28 +1,22 @@
-import { ref } from 'vue';
+import { ref } from "vue";
 
 export default function useTimer(time: number) {
   const timeup = ref(false);
 
-  const timeout = ref(setTimeout(() => {
-    console.log('TIME UP');
-    timeup.value = true
-  }, time));
+  const timeout = ref(setTimeout(() => (timeup.value = true), time));
 
   const stopTimer = () => {
-    console.log('STOP TIMER');
     timeup.value = false;
     clearTimeout(timeout.value);
-  }
+  };
 
   const resetTimer = () => {
-    console.log('RESET TIMER');
     timeup.value = false;
     clearTimeout(timeout.value);
     timeout.value = setTimeout(() => {
-      console.log('TIME UP');
-      timeup.value = true
-    }, time)
-  }
+      timeup.value = true;
+    }, time);
+  };
 
-  return { timeup, stopTimer, resetTimer }
+  return { timeup, stopTimer, resetTimer };
 }
